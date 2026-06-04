1eefc95d610ec36841ea7af989e182705fd1acdb
Marc <walter35.bastian@gmail.com>
2026-06-04 22:40:11 +0200

    feat: redirection login si non connecté sinon accès a la libraire / jeux

---
+++ src/Controller/LibraryController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+#[Route('/library')]
+#[IsGranted('ROLE_USER')]
+final class LibraryController extends AbstractController
+{
+    #[Route('', name: 'app_library')]
+    public function index(): Response
+    {
+        return $this->render('library/index.html.twig');
+    }
+}
