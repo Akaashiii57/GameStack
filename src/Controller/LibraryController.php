debb653b9671115f19250076f0b1c85b3206e081
Marc <walter35.bastian@gmail.com>
2026-06-08 11:56:15 +0200

    fix: Menu conflit "Bibliothèque" et "Jeux"

--- src/Controller/LibraryController.php
+++ src/Controller/LibraryController.php
@@ -7,11 +7,16 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
-#[Route('/library')]
 #[IsGranted('ROLE_USER')]
 final class LibraryController extends AbstractController
 {
-    #[Route('', name: 'app_library')]
+    #[Route('/jeux', name: 'app_jeux')]
+    public function redirectToLibrary(): Response
+    {
+        return $this->redirectToRoute('app_library', [], 301);
+    }
+
+    #[Route('/library', name: 'app_library')]
     public function index(): Response
     {
         return $this->render('library/index.html.twig');
